8e08c6616a7c65ff3e312708b54a6f3e79a6dbfd
Alex Grachev <aleksandr.grachev@fv.dev>
2021-09-24 20:15:53 +0300

    GaugeRegistry добавлено тегирование событий, добавлено событие Consumed

--- src/main/scala/homework/GaugeRegistry.scala
+++ src/main/scala/homework/GaugeRegistry.scala
@@ -12,18 +12,24 @@ object GaugeRegistry {
 
   val name: String = "GaugeRegistry"
 
+  val tagGaugeUpdated:  String = "gaugeUpdated"
+  val tagGaugeConsumed: String = "gaugeConsumed"
+  val tagGaugeDropped:  String = "gaugeDropped"
+
   sealed trait Event
 
   object Event {
-    case class Updated(customerId: UUID, gaugeId: UUID, gauge: Gauge) extends Event
-    case class Dropped(customerId: UUID, gaugeId: UUID) extends Event
+    case class Updated(customerId:  UUID, gaugeId: UUID, gauge: Gauge) extends Event
+    case class Dropped(customerId:  UUID, gaugeId: UUID) extends Event
+    case class Consumed(customerId: UUID, gaugeId: UUID, consumed: Int) extends Event
   }
 
   sealed trait Command
 
   object Command {
-    case class UpdateGauge(gauge: Gauge, replyTo: ActorRef[StatusReply[Event.Updated]]) extends Command
-    case class DropGauge(gaugeId: UUID) extends Command
+    case class UpdateGauge(gauge:  Gauge, replyTo: ActorRef[StatusReply[Event.Updated]]) extends Command
+    case class DropGauge(gaugeId:  UUID) extends Command
+    case class Consume(customerId: UUID, gaugeId: UUID, consumed: Int) extends Command
     case object StopSelf extends Command
   }
 
@@ -47,6 +53,10 @@ object GaugeRegistry {
           commandHandler = (state: State, command: Command) => handleCommand(gaugeId, state, command),
           eventHandler   = (state: State, event: Event) => handleEvent(state, event)
         )
+        .withTagger {
+          case _: Event.Updated => Set(tagGaugeUpdated)
+          case _: Event.Dropped => Set(tagGaugeDropped)
+        }
         .receiveSignal {
           case (state: State, PostStop) =>
             ctx.log.info(s"GaugeRegistry[{}] with state[{}]: Got the PostStop signal.", gaugeId, state)
@@ -60,47 +70,67 @@ object GaugeRegistry {
     customerId:              UUID
   ): ReplyEffect[Event, State] =
     command match {
+
+      case Command.UpdateGauge(gauge, replyTo) if gauge.value >= state.currentValue =>
+        val event: Event.Updated = Event.Updated(customerId = customerId, gaugeId, gauge = gauge)
+        Effect
+          .persist(event)
+          .thenRun { _: State =>
+            ctx.log.debug(s"{} added to journal", event)
+          }
+          .thenReply(replyTo) { _: State =>
+            StatusReply.success(event)
+          }
+
       case Command.UpdateGauge(gauge, replyTo) =>
-        if (gauge.value >= state.currentValue) {
-          val event: Event.Updated = Event.Updated(customerId = customerId, gaugeId, gauge = gauge)
-          Effect
-            .persist(event)
-            .thenRun { _: State =>
-              ctx.log.debug(s"{} added to journal", event)
-            }
-            .thenReply(replyTo) { _: State =>
-              StatusReply.success(event)
-            }
-        } else {
-          Effect.reply(replyTo)(
-            replyError(
-              s"The transmitted gauge reading[${gauge.value}] is less than the current[${state.currentValue}]"
-            )
+        ctx.log.warn(
+          s"The transmitted gauge reading[{}] is less than the current[{}]",
+          gauge.value,
+          state.currentValue
+        )
+        Effect.reply(replyTo)(
+          replyError(
+            s"The transmitted gauge reading[${gauge.value}] is less than the current[${state.currentValue}]"
           )
-        }
+        )
+
+      case Command.Consume(customerId, gaugeId, consumed) =>
+        val event = Event.Consumed(customerId, gaugeId, consumed)
+        Effect
+          .persist(event)
+          .thenRun { _: State =>
+            ctx.log.debug(s"{} added to journal", event)
+          }
+          .thenNoReply()
 
       case cmd @ Command.DropGauge(gaugeId) =>
-        ctx.log.warn(s"{} received", cmd)
+        ctx.log.debug(s"{} received", cmd)
         val event = Event.Dropped(customerId, gaugeId)
         Effect
           .persist(event)
           .thenRun { _: State =>
             ctx.log.debug(s"{} added to journal", event)
-          }.thenStop()
+          }
+          .thenStop()
 
         Effect.noReply
-
 
     }
 
   def handleEvent(state: State, event: Event)(implicit ctx: ActorContext[Command]): State =
     event match {
 
-      case e @ Event.Updated(_, _, gauge) =>
+      case e @ Event.Updated(customerId, gaugeId, gauge) =>
         ctx.log.debug("Event {} received", e)
 
         val newConsumed: Int = gauge.value - state.currentValue
+        ctx.self ! Command.Consume(customerId, gaugeId, newConsumed)
+
         state.copy(consumed = newConsumed, currentValue = gauge.value)
+
+      case e: Event.Consumed =>
+        ctx.log.debug("Event {} received", e)
+        state
 
       case e: Event.Dropped =>
         ctx.log.debug("Event {} received", e)
